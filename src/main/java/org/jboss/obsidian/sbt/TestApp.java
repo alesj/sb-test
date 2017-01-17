a19627783b73cb0f245ba50f8371d611a93b3498
Ales Justin <ales.justin@gmail.com>
2017-01-17 15:14:19 +0100

    Simple Artemis example.

---
+++ src/main/java/org/jboss/obsidian/sbt/TestApp.java
@@ -0,0 +1,14 @@
+package org.jboss.obsidian.sbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+@SpringBootApplication
+public class TestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApp.class, args);
+    }
+}
