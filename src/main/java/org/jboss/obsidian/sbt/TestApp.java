50c40128c5aa31e33070db9ebdb68b0ab6da46b5
Ales Justin <ales.justin@gmail.com>
2017-08-03 14:45:15 +0200

    Use OT java-cloud + Jaeger.

--- src/main/java/org/jboss/obsidian/sbt/TestApp.java
+++ src/main/java/org/jboss/obsidian/sbt/TestApp.java
@@ -1,20 +1,12 @@
 package org.jboss.obsidian.sbt;
 
-import brave.Tracing;
-import brave.opentracing.BraveTracer;
+import com.uber.jaeger.Configuration;
+import com.uber.jaeger.samplers.ProbabilisticSampler;
 import io.opentracing.Tracer;
-import io.opentracing.mock.MockSpan;
-import io.opentracing.mock.MockTracer;
-import io.opentracing.util.ThreadLocalActiveSpanSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jms.support.JmsHeaderMapper;
-import zipkin.Span;
-import zipkin.reporter.AsyncReporter;
-import zipkin.reporter.Sender;
-import zipkin.reporter.okhttp3.OkHttpSender;
 
 /**
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
@@ -39,42 +31,44 @@ public class TestApp {
     }
 */
 
-  @Bean
-  public JmsHeaderMapper mapper() {
-    return TracingJmsHeaderMapper.braveHeaderMapper();
-  }
-
-    @Bean
-    public JmsListenerAspect createJmsListenerAspect() {
-        return new JmsListenerAspect();
-    }
-
     //@Bean
-    public Tracer createMockTracer() {
-        return new MockTracer(new ThreadLocalActiveSpanSource(), MockTracer.Propagator.TEXT_MAP) {
-            @Override
-            protected void onSpanFinished(MockSpan mockSpan) {
-                System.out.println(mockSpan);
-            }
-        };
-    }
+//    public Tracer createMockTracer() {
+//        return new MockTracer(new ThreadLocalActiveSpanSource(), MockTracer.Propagator.TEXT_MAP) {
+//            @Override
+//            protected void onSpanFinished(MockSpan mockSpan) {
+//                System.out.println(mockSpan);
+//            }
+//        };
+//    }
 
     @Bean
-    public Tracer createZipkinTracer() {
-        // Configure a reporter, which controls how often spans are sent
-        //   (the dependency is io.zipkin.reporter:zipkin-sender-okhttp3)
-        String url = String.format("http://%s:%s/api/v1/spans", zipkinHost, zipkinPort);
-        Sender sender = OkHttpSender.create(url);
-        AsyncReporter<Span> reporter = AsyncReporter.builder(sender).build();
-
-        // Now, create a Brave tracing component with the service name you want to see in Zipkin.
-        //   (the dependency is io.zipkin.brave:brave)
-        brave.Tracing braveTracing = Tracing.newBuilder()
-            .localServiceName("sb-test")
-            .reporter(reporter)
-            .build();
-
-        // use this to create an OpenTracing Tracer
-        return BraveTracer.create(braveTracing);
+    public Tracer createJaegerTracer() {
+//      Reporter reporter = new InMemoryReporter();
+//      Sampler sampler = new ConstSampler(true);
+//      return new com.uber.jaeger.Tracer.Builder("sb-test", reporter, sampler).build();
+      return new Configuration(
+        "sb-test",
+        new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
+        new Configuration.ReporterConfiguration()
+      ).getTracer();
     }
+
+//    @Bean
+//    public Tracer createZipkinTracer() {
+//        // Configure a reporter, which controls how often spans are sent
+//        //   (the dependency is io.zipkin.reporter:zipkin-sender-okhttp3)
+//        String url = String.format("http://%s:%s/api/v1/spans", zipkinHost, zipkinPort);
+//        Sender sender = OkHttpSender.create(url);
+//        AsyncReporter<Span> reporter = AsyncReporter.builder(sender).build();
+//
+//        // Now, create a Brave tracing component with the service name you want to see in Zipkin.
+//        //   (the dependency is io.zipkin.brave:brave)
+//        brave.Tracing braveTracing = Tracing.newBuilder()
+//            .localServiceName("sb-test")
+//            .reporter(reporter)
+//            .build();
+//
+//        // use this to create an OpenTracing Tracer
+//        return BraveTracer.create(braveTracing);
+//    }
 }
